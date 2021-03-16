8103f24bcdeeed46f73d24d04aa1c5d103d45ed8
A.Popartaru <popartaru.alexandru@gmail.com>
2021-03-16 12:54:29 +0100

    Fixed tags bean root element name

--- xroads-module-rewix/src/main/java/it/zero11/xroads/modules/rewix/api/model/ProductTagsBean.java
+++ xroads-module-rewix/src/main/java/it/zero11/xroads/modules/rewix/api/model/ProductTagsBean.java
@@ -8,7 +8,7 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "productTaxables")
+@XmlRootElement(name = "productTags")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ProductTagsBean {
 	@XmlAttribute
